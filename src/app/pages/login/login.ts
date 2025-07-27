03876c002afe416671e312a848d0e22659eece39
ceciliascm <ceciliascm@gmail.com>
2025-07-27 20:59:36 -0300

    build: (front-end: fix visual elements) (back-end: implement login service to authenticate user and store token in sessionStorage)

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import {DefaultLoginLayout} from '../../components/default-login-layout/default-login-layout';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {PrimaryInput} from '../../components/primary-input/primary-input';
+import {Router} from '@angular/router';
+import {LoginResponse} from '../../types/login-response.type';
+import {LoginService} from '../../services/login-service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   imports: [
     DefaultLoginLayout,
     ReactiveFormsModule,
     PrimaryInput
+  ],
+  providers: [
+    LoginService
   ],
   selector: 'app-login',
   styleUrl: './login.css',
@@ -16,11 +23,26 @@ import {PrimaryInput} from '../../components/primary-input/primary-input';
 export class Login {
   loginForm: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
   }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login successfully"),
+      error: () => this.toastService.error("Login failed.")
+    })
+  }
+
+  /* navigate() {
+    this.router.navigate(["/"signup])
+  } */
 
 }
